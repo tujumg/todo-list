f451b3f6513a97ece532486081b08da78daadb30
tujumg <tujumg@gmail.com>
2022-05-12 22:48:27 +0800

    feat:add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const todoSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    }
+})
+module.exports = mongoose.model('Todo',todoSchema)
+
 const app = express()
+
+mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error',()=>{
+    console.log('mongodb error!')
+})
+db.once('open',()=>{
+    console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
     res.send('hello world')
