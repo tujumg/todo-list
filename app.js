3ec4038673c0df989ab624cbc3b75b9611b5d2e1
tujumg <tujumg@gmail.com>
2022-05-13 08:14:31 +0800

    no message

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const todoSchema = new Schema({
-    name:{
-        type:String,
-        required:true
-    }
-})
-module.exports = mongoose.model('Todo',todoSchema)
 
 const app = express()
 
---
+++ models/seeds/todoSeeder.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Todo = require('../todo') // 載入 todo model
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+    console.log('mongodb error!')
+})
+db.once('open', () => {
+    console.log('mongodb connected!')
+
+    for (let i = 0 ; i < 10 ; i++){
+        Todo.creat({name:`name-${i}`})
+    }
+    console.log('done')
+})
