9c8f0128cce3526f58b308eff7bdbaa261e9fe35
tujumg <tujumg@gmail.com>
2022-05-13 08:26:52 +0800

    todo seeder done

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -9,7 +9,7 @@ db.once('open', () => {
     console.log('mongodb connected!')
 
     for (let i = 0 ; i < 10 ; i++){
-        Todo.creat({name:`name-${i}`})
+        Todo.create({name:`name-${i}`})
     }
     console.log('done')
 })
