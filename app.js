4d44f7d6be91d4c772405035ea22bb39155e099e
tujumg <tujumg@gmail.com>
2022-05-24 18:27:08 +0800

    no message

--- app.js
+++ app.js
@@ -1,82 +1,20 @@
 const express = require('express')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const Todo = require('./models/todo')
+// const Todo = require('./models/todo')
 const bodyParser = require('body-parser')
+const methodOverride =require('method-override')
+// 引用路由器
+const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 
-mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error',()=>{
-    console.log('mongodb error!')
-})
-db.once('open',()=>{
-    console.log('mongodb connected!')
-})
-
-// app.get('/', (req, res) => {
-//     res.send('hello world')
-// })
-
 app.engine('hbs', exphbs({ defaultLayout : 'main' , extname: '.hbs' }))
 app.set('view engine' , 'hbs')
-
 app.use(bodyParser.urlencoded({extended:true}))
-
-app.get('/', (req, res)=>{
-    Todo.find() // 取出 Todo model 裡的所有資料
-        .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-        .then(todos => res.render('index',{todos}))  // 將資料傳給 index 樣板
-        .catch(error=>console.error(error)) // 錯誤處理
-})
-app.get('/todos/new',(req,res) => {
-    return res.render('new')
-})
-app.post('/todos',(req,res) =>{
-    const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-    return Todo.create({ name })     // 存入資料庫
-        .then(() => res.redirect('/')) // 新增完成後導回首頁
-        .catch(error => console.log(error))
-})
-
-app.get('/todos/:id', (req,res)=>{
-    const id = req.params.id
-    return Todo.findById(id)
-        .lean()
-        .then((todo)=>res.render('detail',{todo}))
-        .catch(error =>console.log(error))
-})
-
-app.get('/todos/:id/edit', (req,res)=>{
-    const id = req.params.id
-    return Todo.findById(id)
-        .lean()
-        .then((todo)=>res.render('edit',{todo}))
-        .catch(error =>console.log(error))
-})
-
-app.post('/todos/:id/edit',(req,res) =>{
-    const id = req.params.id 
-    const name = req.body.name      // 從 req.body 拿出表單裡的 name 資料
-    return Todo.findById(id)     // 存入資料庫
-        .then(todo => {
-            todo.name = name
-            return todo.save()
-        })
-        .then(()=> res.redirect(`/todos/${id}`))
-        .catch(error => console.log(error))
-})
-
-app.post('/todos/:id/delete',(req,res) =>{
-    const id = req.params.id
-    return Todo.findById(id)
-        .then(todo => todo.remove())
-        .then(() => res.redirect('/'))
-        .catch(error => console.log(error))
-})
+app.use(methodOverride('_method'))
+app.use(routes)       // 將 request 導入路由器
 
 app.listen(3000, () => {
     console.log('App is running on http://localhost:3000')
--- models/todo.js
+++ models/todo.js
@@ -4,6 +4,10 @@ const todoSchema = new Schema({
     name:{
         type:String,
         required:true
+    },
+    isDone:{
+        type: Boolean,
+        default: false // 預設完成狀態為 false
     }
 })
 module.exports = mongoose.model('Todo',todoSchema)
