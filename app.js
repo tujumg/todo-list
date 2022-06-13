459be151cbadca8e2d14528cc0e5bc22a7fe53af
tujumg <tujumg@gmail.com>
2022-06-13 17:19:25 +0800

    add express-session

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ const routes = require('./routes') // 引用路由器
 require('./config/mongoose')
 const PORT = process.env.PORT || 3000
 const app = express()
+const session = require('express-session')
 
 app.engine('hbs', exphbs({ defaultLayout : 'main' , extname: '.hbs' }))
 app.set('view engine' , 'hbs')
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
 app.use(routes)       // 將 request 導入路由器
+app.use(session({
+    secret:'ThisIsMySecret',
+    resave:false,
+    saveUninitialized:true
+}))
 
 app.listen(PORT, () => {
     console.log(`App is running on http://localhost:${PORT}`)
