f3d740b12b1f9982e07d1fa39ae71da9ba066876
tujumg <tujumg@gmail.com>
2022-06-17 23:38:26 +0800

    add message partials

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000
 const app = express()
 const session = require('express-session')
 const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 
 app.engine('hbs', exphbs({ defaultLayout : 'main' , extname: '.hbs' }))
 app.set('view engine' , 'hbs')
@@ -19,10 +20,13 @@ app.use(session({
     saveUninitialized:true
 }))
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
     // 你可以在這裡 console.log(req.user) 等資訊來觀察
     res.locals.isAuthenticated = req.isAuthenticated()
     res.locals.user = req.user
+    res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+    res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
     next()
 })
 app.use(routes)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -12,18 +12,40 @@ router.post('/login', passport.authenticate('local', {
 }))
 router.get('/logout',(req,res)=>{
     req.logout()
+    req.flash('success_msg', '你已經成功登出。')
     res.render('/users/login')
 })
 router.get('/register',(req,res) => {
     res.render('register')
 })
 router.post('/register',(req,res)=>{
-    const {name,email,password,confirmPassword} = req.body
-    console.log(password)
+    const { name, email, password, confirmPassword } = req.body
+    const errors = []
+    if (!name || !email || !password || !confirmPassword) {
+        errors.push({ message: '所有欄位都是必填。' })
+    }
+    if (password !== confirmPassword) {
+        errors.push({ message: '密碼與確認密碼不相符！' })
+    }
+    if (errors.length) {
+        return res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            confirmPassword
+        })
+    }
     User.findOne({ email }).then( user => {
         if(user){
-            console.log('User already exists')
-            res.render('register', {name,email,password,confirmPassword})
+            errors.push({ message: '這個 Email 已經註冊過了。' })
+            return res.render('register', {
+                errors,
+                name,
+                email,
+                password,
+                confirmPassword
+            })
         } else{
             return User.create({
                 name,
